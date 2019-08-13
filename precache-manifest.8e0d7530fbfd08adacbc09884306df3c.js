82099cedf54d6c63ba608a928271b1a6d147ae5c
buzzyun <buzzyun@gmail.com>
2019-08-13 23:54:46 +0900

    Updates

---
+++ precache-manifest.8e0d7530fbfd08adacbc09884306df3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2403d8f7ef43a82dfe7cca89e8dd4d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "e73b0894ff511bd1d69b",
+    "url": "/movie_app/static/css/main.03c8b624.chunk.css"
+  },
+  {
+    "revision": "eef33fb471bd3541426c",
+    "url": "/movie_app/static/js/2.cd6656c6.chunk.js"
+  },
+  {
+    "revision": "e73b0894ff511bd1d69b",
+    "url": "/movie_app/static/js/main.29d0cacf.chunk.js"
+  },
+  {
+    "revision": "f9c5d4d9cea5f935b75a",
+    "url": "/movie_app/static/js/runtime~main.b05fbdd8.js"
+  }
+]);
