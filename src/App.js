d0e6b9c9bf73c03f3d8b01be66733a65f10a2dc8
buzzyun <buzzyun@gmail.com>
2019-08-13 11:15:54 +0900

    #2 work company

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+
+function Food(props){
+
+  return (
+    <h3>I Like {props.favorite}</h3>
+  );
+
+}
+
 function App() {
   return (
-    <div ><h1>Hello!!</h1></div>
+    <div >
+      <h1>Hello!!</h1>   
+      <Food favorite = "kimchi"/>   
+      <Food favorite = "chicken"/>
+      <Food favorite = "buger"/>
+    </div>    
   );
 }
 
